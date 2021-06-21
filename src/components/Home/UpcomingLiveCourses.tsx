9b18e366daf45bf7960b9f77bd4532ec8119dd45
afif <afifahmed456123@gmail.com>
2021-06-22 01:55:22 +0530

    refactor: :hammer: refactoring styles and fixing some bugs

--- src/components/Home/UpcomingLiveCourses.tsx
+++ src/components/Home/UpcomingLiveCourses.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import RenderWebinarCard from './../Card/CourseCard';
-import {Typography,Box,Container} from '@material-ui/core';
+import React from "react";
+import RenderWebinarCard from "./../Card/CourseCard";
+import { Typography, Box, Container } from "@material-ui/core";
 export default function PopularCourses() {
-    return (
-        <div>
-            <Box >
-                <Box style={{display:"flex",justifyContent:"space-between"}}>
-                    <Typography  variant="h5" style={{display:"flex",justifyContent:'left', alignItems:'left',marginLeft:'55px',marginTop:'60px',color:'black',marginBottom:'20px'}}>
-                    Upcoming<span style={{fontWeight:600}}>&nbsp;Live Courses</span> 
-                    </Typography> 
-                </Box>
-            
-            </Box>
-<Container style={{marginLeft:'5px'}}>
-        <RenderWebinarCard />
-</Container>
-  
-</div>
-    )
+	return (
+		<div>
+			<Box>
+				<Box style={{ display: "flex", justifyContent: "space-between" }}>
+					<Typography
+						variant='h5'
+						style={{
+							display: "flex",
+							justifyContent: "left",
+							alignItems: "left",
+							marginLeft: "55px",
+							marginTop: "60px",
+							color: "black",
+							marginBottom: "20px",
+						}}
+					>
+						Upcoming<span style={{ fontWeight: 600 }}>&nbsp;Live Courses</span>
+					</Typography>
+				</Box>
+			</Box>
+			<Container style={{ marginLeft: "5px" }}>
+				<RenderWebinarCard />
+			</Container>
+		</div>
+	);
 }
